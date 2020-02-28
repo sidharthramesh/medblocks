0d94c567c64f7df02d23aed970ea2ec14629b6ef
Sidharth R <tornadoalert@gmail.com>
2020-02-28 16:47:55 +0530

    bug fixes

--- medblocks/workers.py
+++ medblocks/workers.py
@@ -4,13 +4,14 @@ from nameko.standalone.rpc import ClusterRpcProxy
 from nameko.web.handlers import http
 from nameko.events import EventDispatcher, event_handler
 from nameko.timer import timer
+from nameko.rpc import rpc
 import logging
 import requests
 class HttpServer:
     name = "http_service"
     @http("GET", "/")
     def version(self, request):
-        return json.dumps({"version": "Medblocks v1"})
+        return json.dumps({"version": settings.VERSION})
 
 class DatabaseService:
     # 2 way Replications from iptable
@@ -34,27 +35,32 @@ class DatabaseService:
         db = "data"
         localdb = "medblocksSync"
         last_seq = requests.get(f"{settings.COUCHDB_URL}/{db}/_local/{localdb}").json()
-        if last_seq["error"]:
-            last_seq = None
-        else:
+        try:
             last_seq = last_seq["last_seq"]
+        except KeyError:
+            last_seq = None
         if last_seq is not None:
             url = f"{settings.COUCHDB_URL}/{db}/_changes?since={last_seq}"
         else:
             url = "http://db:5984/data/_changes"
         changes = requests.get(url).json()
-        for change in changes:
-            seq = change["seq"]
-            id = change["id"]
-        with ClusterRpcProxy(config={"AMQP_URI": settings.AMQP_URL}):
-            # Async call to minio service with id
-            result = pass
-            pass
-        r = requests.put(f"{settings.COUCHDB_URL}/{db}/_local/{localdb}", json={"last_seq":seq})
+        if len(changes) > 0:
+            results = []
+            for change in changes:
+                if change.get("deleted"):
+                    continue
+                seq = change["seq"]
+                id = change["id"]
+                logging.info(f"Change found for data: {id}")
+            with ClusterRpcProxy(config={"AMQP_URI": settings.AMQP_URL}):
+                # Async call to minio service with id
+                logging.info(f"Triggering upload to s3 for {id}")
+                result = None
+                results.append(result)
+            # Wait for all results to complete
+            
+            r = requests.put(f"{settings.COUCHDB_URL}/{db}/_local/{localdb}", json={"last_seq":seq})
 
-        print(changes["last_seq"])
-        
-        print(r.json())
     
     def txChanges(self):
         pass
@@ -65,7 +71,6 @@ class DatabaseService:
 
         # Trigger event
         pass
-
     def setupReplications(self, ipAddress):
         """Set up replications"""
         # Exculde localhost, 127.0.0.1 etc
@@ -75,7 +80,8 @@ class DatabaseService:
 
 
 class BlobDataService:
-    def dataUploadWorker(self, id):
+    @rpc
+    def dataUpload(self, id):
         # Look at attachments in data database
         # Upload to S3
         # Make update to document: - detele attachment and set uploaded: true
